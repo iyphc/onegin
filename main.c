fca2b295393d95416092d42e7628f62d215f9181
Wohrus <werhistudy@yandex.ru>
2021-12-02 23:45:44 +0300

    Updated output and running of tests

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "onegin.h"
+#include "onegin_test.h"
 #include "string.h"
 //ftell, fseek, fread
 
-int comp(const void* p1, const void* p2) {
-	return myStrcmp(*(const char**)p1, *(const char**)p2);
-}
+/**  
+ * @file main.c
+ * @brief Contains main function
+*/
 
 int main() {
 	//Read file
@@ -12,15 +14,24 @@ int main() {
 	int file_byte_size = count_symbols(input);
 	//Convert file to char* array
 	char* file_buffer = fileToString(input, file_byte_size);
+	//Split one char* array in a two-dimensional array of strings
 	int lines_count = (int)count_lines(file_buffer, file_byte_size);
-	//Split one char* array int a two-dimensional array of strings
-	char** strings = (char**)calloc(lines_count, sizeof(char*));
-	divideString(file_buffer, strings, file_byte_size);
-	//sort two-dimesional array
-	qsort(strings, lines_count, sizeof(char*), comp);
-	//Вывод
-	for(int i = 0; i < lines_count; i++) {
+	int actual_lines_count = formatString(file_buffer, file_byte_size);
+	char** strings = (char**)calloc(actual_lines_count, sizeof(char*));
+	strings = buildString(file_buffer, strings, file_byte_size);
+	//sorting two-dimesional array
+	qsort(strings, actual_lines_count, sizeof(char*), comp);
+	//output
+	printf("The number of bytes in the file = %d\n\n", file_byte_size);
+	printf("Initial number of lines = %d\n\n", lines_count);
+	printf("Actual number of lines = %d\n\n", actual_lines_count);
+	for(int i = 0; i < actual_lines_count; i++) {
 		printf("%s\n", strings[i]);
 	}
+	run_all_tests();
+	free(strings);
+	free(file_buffer);
+	fclose(input);
+	printf("\nНеужели мы справились?\nCongratulations, all tests passed!\n\n");
 	return 0;
 }
